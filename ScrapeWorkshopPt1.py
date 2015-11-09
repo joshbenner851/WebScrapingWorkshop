c4f0b9947050a1d88d7295f57250d05f22006608
Joshua Benner <bennerjo@cse.msu.edu>
2015-11-09 02:11:27 -0500

    Part 1 of workshop

---
+++ ScrapeWorkshopPt1.py
@@ -0,0 +1,23 @@
+import urllib.request
+import urllib.parse
+from bs4 import BeautifulSoup
+
+
+url = "https://eatatstate.com/menus/brody"
+req = urllib.request.Request(url, None,headers={'User-Agent' : 'Mozilla/5.0'})
+html = urllib.request.urlopen(req)
+soup = BeautifulSoup(html)
+
+date = soup.find_all('div',class_="date-heading")
+viewContent = soup.find_all("div",class_="view-content")[1]
+
+for tables in viewContent.contents:
+    if tables and tables != "/n":
+        body = tables.contents[5]
+        oddView = [x for x in body.contents if x!='\n']
+        print(oddView)
+        
+
+
+
+
